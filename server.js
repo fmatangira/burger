ab61452e72a3a565629f6dc269ffea3c94bf7fff
Farai Matangira <fmatangira@gmail.com>
2020-01-20 22:54:43 -0600

    Got css to work in html render

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
 const db = require('./models');
-// const main = require('../views/main.js');
 
+// Use the express.static middleware to serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 require('./controllers/html-routes.js')(app)
 
@@ -13,7 +17,7 @@ db.sequelize.sync().then(()=>{
     
     app.listen(PORT,()=>{
 
-        console.log(`Localhost is listening on port: ${PORT}`);
+        console.log(`Localhost server is listening on port: ${PORT}`);
         
     })
 
--- views/main.js
+++ views/main.js
@@ -9,7 +9,7 @@ const render = function({name, devour, createdAt}) {
         <title>BURGER Database</title>
 
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <link rel="stylesheet" href="../public/css/styles.css">
+        <link rel="stylesheet" href="/css/styles.css">
 
     </head>
     <body>
